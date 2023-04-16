bd5f622b0858b6e6b1d3e824d2bd8f4816d7ad88
Azure-hang <113017330+Azure-hang@users.noreply.github.com>
2023-04-16 15:15:13 +0800

    Update the script file
    Added some script files, summary of the script, as well as music playback, pause, and skip.

---
+++ Assets/Zihang Yu/Script update/All.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class All : MonoBehaviour
+{
+    private AudioSource au;
+
+    //public AudioClip bgaudioClip;
+    //private AudioSource audio;
+
+    void Start(){
+        au = GetComponent<AudioSource>();
+    }
+
+
+    public void Close()
+    {
+        Application.Quit();//退出应用
+    }
+
+    public void ButtononClick1(){
+        Application.LoadLevel("p1");
+    }
+
+    public void ButtononClick2(){
+        Application.LoadLevel("p2");
+    }
+
+    public void ButtononClick3(){
+        Application.LoadLevel("p3");
+    }
+
+    public void ButtononClick4(){
+        Application.LoadLevel("p4");
+    }
+
+    public void ButtononClick0(){
+        Application.LoadLevel("start");
+    }
+
+    public void OnExitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+
+    public void Clickfalse()
+    {
+        gameObject.SetActive(false);
+    }
+
+     public void Clickopen()
+    {
+        gameObject.SetActive(true);
+    }
+
+    // public void soundPlay{
+    //     if(au.Play()){
+    //         au.Stop();
+    //     }else{
+    //         au.Play();
+    //     }
+    // }
+
+}
