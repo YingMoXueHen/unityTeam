74405048f4e319f77bb43a96097543fd31a28048
YingMoXueHen <1156657431@qq.com>
2023-04-17 21:21:55 +0800

    Add script to main Menu
    Add script to main Menu

---
+++ Assets/MainMenu/Script/Porial2.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Porial2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(new Vector3 (534.4457f,6.414518f, 516.1212f), new Vector3 (0f, 1f, 0f), 0.3f); 
+    }
+}
